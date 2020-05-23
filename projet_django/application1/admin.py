a58a4ef9bd73653f098df7627b39c5e88ac373a6
Philippine <philippine.renaudin@gmail.com>
2020-05-23 11:18:00 +0200

    Delete admin.py

--- projet_django/application1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Airports_dep, Airports_arr, Incidents, Incidents_description
-
-admin.site.register(Airports_dep)
-admin.site.register(Airports_arr)
-admin.site.register(Incidents)
-admin.site.register(Incidents_description)
-
